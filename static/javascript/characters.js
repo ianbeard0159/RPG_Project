9476da0e7db713956f19a5a2476d38fc596dd970
jsablaon <jsablaon644@gmail.com>
2021-12-04 13:01:50 -0800

    populate select and game pages

--- static/javascript/characters.js
+++ static/javascript/characters.js
@@ -20,8 +20,39 @@ function generateHTML(inData, enemyData) {
             inData[entry].agility,
             inData[entry].char_level,
             inData[entry].health_ratio,
+            inData[entry].essence_ratio,
             inData[entry].ap_ratio
             );
+
+        // Make a request for the attacks used for that unit
+        const xhttpAttacks = new XMLHttpRequest();
+        // Make sure the response is syncronous
+        xhttpAttacks.open('GET', `${url}/getActions/characters/attacks/${inData[entry]['id_characters']}`, false);
+        xhttpAttacks.onload = function () {
+            // Parse the data from the response
+            const attacks = JSON.parse(xhttpAttacks.responseText);
+            // If there are any entries in the response
+            for (let attack in attacks){
+                // Make rows in the table for the attacks
+                characterList[inData[entry].char_name].attackList[attacks[attack].attack_name] = attacks[attack];
+            }
+        }
+        xhttpAttacks.send();
+
+                // Make a request for the supports used for that unit
+                const xhttpSupports = new XMLHttpRequest();
+                // Make sure the response is syncronous
+                xhttpSupports.open('GET', `${url}/getActions/characters/supports/${inData[entry]['id_characters']}`, false);
+                xhttpSupports.onload = function () {
+                    // Parse the data from the response
+                    const supports = JSON.parse(xhttpSupports.responseText);
+                    // If there are any entries in the response
+                    for (let support in supports){
+                        // Make rows in the table for the supports
+                        characterList[inData[entry].char_name].supportList[supports[support].support_name] = supports[support];
+                    }
+                }
+                xhttpSupports.send();
     }
     game.populateCharacters(characterList);
 
@@ -38,19 +69,78 @@ function generateHTML(inData, enemyData) {
         enemyData[entry].health_ratio,
         enemyData[entry].ap_ratio
         );
+
+        // Make a second request for the attacks used for that unit
+        const xhttpAttacks = new XMLHttpRequest();
+        // Make sure the response is syncronous
+        xhttpAttacks.open('GET', `${url}/getActions/enemys/attacks/${enemyData[entry]['id_enemys']}`, false);
+        xhttpAttacks.onload = function () {
+            // Parse the data from the response
+            const attacks = JSON.parse(xhttpAttacks.responseText);
+            // If there are any entries in the response
+            for (let attack in attacks){
+                // Make rows in the table for the attacks
+                enemyList[enemyData[entry].enemy_name].attackList[attacks[attack].attack_name] = attacks[attack];
+            }
+        }
+        xhttpAttacks.send();
+
+                // Make a second request for the supports used for that unit
+                const xhttpSupports = new XMLHttpRequest();
+                // Make sure the response is syncronous
+                xhttpSupports.open('GET', `${url}/getActions/enemys/supports/${enemyData[entry]['id_enemys']}`, false);
+                xhttpSupports.onload = function () {
+                    // Parse the data from the response
+                    const supports = JSON.parse(xhttpSupports.responseText);
+                    // If there are any entries in the response
+                    for (let support in supports){
+                        // Make rows in the table for the supports
+                        enemyList[enemyData[entry].enemy_name].supportList[supports[support].support_name] = supports[support];
+                    }
+                }
+                xhttpSupports.send();
     }
 
     game.populateEnemys(enemyList);
     
     for (let char in game.characterList) {  
         output += `<table id=${char} style="margin: 10px; padding: 5px; background-color: lightgray;">\
-            <th>${char} </th>\
-            <tr><td>${game.characterList[char].description}</tr></td></table>`;
+            <th>${char} </th>`;
+        // Display all character fields
+        for (let field in game.characterList[char]) {
+            if (field != 'id_characters') {
+                output += `<tr>\
+                <td>${field}</td>\
+                <td>${game.characterList[char][field]}</td>\
+                </tr>`;
+            }
+        }
+        // Display character attacks and supports
+        for (let attack in game.characterList[char].attackList){
+            output += `<th>${attack} </th>\
+            <tr><td>${game.characterList[char].attackList[attack].description}</tr></td>`;
+        }
+        for (let support in game.characterList[char].supportList){
+            output += `<th>${support} </th>\
+            <tr><td>${game.characterList[char].supportList[support].description}</tr></td>`;
+        }
+        output += '</table>';
     }
+
+    // Display enemy information
     for (let enemy in game.enemyList) {   
         output += `<table id=${enemy} style="margin: 10px; padding: 5px; background-color: lightgray;">\
             <th>${enemy} </th>\
-            <tr><td>${game.enemyList[enemy].description}</tr></td></table>`;
+            <tr><td>${game.enemyList[enemy].description}</tr></td>`;
+        for (let attack in game.enemyList[enemy].attackList){
+            output += `<th>${attack} </th>\
+            <tr><td>${game.enemyList[enemy].attackList[attack].description}</tr></td>`;
+        }
+        for (let support in game.enemyList[enemy].supportList){
+            output += `<th>${support} </th>\
+            <tr><td>${game.enemyList[enemy].supportList[support].description}</tr></td>`;
+        }
+        output += `</table>`;
     }
     return output;
 }
--- static/javascript/select.js
+++ static/javascript/select.js
@@ -3,17 +3,19 @@ const url = "http://localhost:3000";
 function generateHTML(inData) {
     let output = '';
     for (let entry in inData) {        
-        output += `<table id=${entry} style="margin: 10px; padding: 5px; background-color: lightgray;">\
-            <th>${entry} </th>\
+        output += `<table id=${inData[entry].char_name} style="margin: 10px; padding: 5px; background-color: lightgray;">\
+            <th>${inData[entry].char_name} </th>\
             <tr><td>${inData[entry].description}</tr></td>`;
     }
     return output;
 }
 
 window.onload = function () {
-    console.log(`${url}/getNames/:unitTable`);
+    console.log(`${url}/getNames/characters`);
+    //console.log(`${url}/test/table/characters`);
     const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', `${url}/getNames/:unitTable`);
+    xhttp.open('GET', `${url}/getNames/characters`);
+    //xhttp.open('GET', `${url}/test/table/characters`);
     xhttp.onload = function () {
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
